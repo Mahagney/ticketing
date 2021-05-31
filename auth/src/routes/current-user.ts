ab3c26618a26cea6b00abe5de0fd330274250f63
Saleh Mahagney <saleh@Salehs-MacBook-Pro.local>
2021-05-31 11:48:47 +0300

    add routes

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentUser',(req, res)=>{ res.send('Hi there!')})
+
+export { router as currentUserRouter};
